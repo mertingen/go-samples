81391b4c55757d984dae61e9b9cf8cb2d69a740d
Mert Simsek <mert.simsek@beyn.com.tr>
2022-01-25 13:09:43 +0300

    Students method responses are formated

--- fiber-crud/handlers/student.go
+++ fiber-crud/handlers/student.go
@@ -6,58 +6,117 @@ import (
 	"github.com/mertingen/go-samples/entities"
 )
 
-func GetStudents(c *fiber.Ctx) error {
-	var student []entities.Student
+type response struct {
+	Status  bool               `json:"status"`
+	Message string             `json:"message"`
+	Data    []entities.Student `json:"data"`
+}
 
-	config.Database.Find(&student)
-	return c.Status(200).JSON(student)
+func GetStudents(c *fiber.Ctx) error {
+	students := make([]entities.Student, 0)
+
+	result := config.Database.Find(&students)
+
+	resp := response{
+		Status:  true,
+		Message: "Students are found.",
+		Data:    students,
+	}
+	if result.RowsAffected == 0 {
+		resp.Status = false
+		resp.Message = "Students are not found."
+		return c.Status(200).JSON(resp)
+	}
+	return c.Status(200).JSON(resp)
 }
 
 func GetStudent(c *fiber.Ctx) error {
 	id := c.Params("id")
-	var student entities.Student
+	student := make([]entities.Student, 0)
 
 	result := config.Database.Find(&student, id)
 
+	resp := response{
+		Status:  true,
+		Message: "Student is found.",
+		Data:    student,
+	}
 	if result.RowsAffected == 0 {
-		return c.SendStatus(404)
+		resp.Status = false
+		resp.Message = "Students is not found."
+		return c.Status(200).JSON(resp)
 	}
 
-	return c.Status(200).JSON(&student)
+	return c.Status(200).JSON(&resp)
 }
 
 func AddStudent(c *fiber.Ctx) error {
 	student := new(entities.Student)
 
+	resp := response{
+		Status:  true,
+		Message: "Student is created.",
+		Data:    make([]entities.Student, 0),
+	}
 	if err := c.BodyParser(student); err != nil {
-		return c.Status(503).SendString(err.Error())
+		resp.Status = false
+		resp.Message = "An error occurs while player is created."
+		return c.Status(200).JSON(resp)
 	}
 
-	config.Database.Create(&student)
-	return c.Status(201).JSON(student)
+	result := config.Database.Create(&student)
+	if result.RowsAffected == 0 {
+		resp.Status = false
+		resp.Message = "An error occurs while player is created."
+		return c.Status(200).JSON(resp)
+	}
+
+	resp.Data = append(resp.Data, *student)
+	return c.Status(201).JSON(resp)
 }
 
 func UpdateStudent(c *fiber.Ctx) error {
 	student := new(entities.Student)
 	id := c.Params("id")
 
+	resp := response{
+		Status:  true,
+		Message: "Student is updated.",
+		Data:    make([]entities.Student, 0),
+	}
 	if err := c.BodyParser(student); err != nil {
-		return c.Status(503).SendString(err.Error())
+		resp.Status = false
+		resp.Message = "An error occurs while player is updated."
+		return c.Status(200).JSON(resp)
 	}
 
-	config.Database.Where("id = ?", id).Updates(&student)
-	return c.Status(200).JSON(student)
+	result := config.Database.Where("id = ?", id).Updates(&student)
+	if result.RowsAffected == 0 {
+		resp.Status = false
+		resp.Message = "An error occurs while player is updated."
+		return c.Status(200).JSON(resp)
+	}
+
+	resp.Data = append(resp.Data, *student)
+	return c.Status(200).JSON(resp)
 }
 
 func RemoveStudent(c *fiber.Ctx) error {
 	id := c.Params("id")
 	var student entities.Student
 
+	resp := response{
+		Status:  true,
+		Message: "Student is deleted.",
+		Data:    make([]entities.Student, 0),
+	}
 	result := config.Database.Delete(&student, id)
 
 	if result.RowsAffected == 0 {
-		return c.SendStatus(404)
+		resp.Status = false
+		resp.Message = "An error occurs while player is updated."
+		return c.Status(200).JSON(resp)
 	}
 
-	return c.SendStatus(200)
+	return c.Status(200).JSON(resp)
 }
--- fiber-crud/main.go
+++ fiber-crud/main.go
@@ -7,10 +7,19 @@ import (
 	"github.com/mertingen/go-samples/handlers"
 	"log"
 	"os"
+	"os/signal"
 )
 
 func main() {
 	app := fiber.New()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		_ = <-c
+		fmt.Println("Gracefully shutting down...")
+		_ = app.Shutdown()
+	}()
 
 	err := config.Connect()
 	if err != nil {
@@ -23,6 +32,7 @@ func main() {
 	app.Put("/students/:id", handlers.UpdateStudent)
 	app.Delete("/students/:id", handlers.RemoveStudent)
 
-	log.Fatal(app.Listen(fmt.Sprintf(":%s", os.Getenv("APP_PORT"))))
-
+	if err := app.Listen(fmt.Sprintf(":%s", os.Getenv("APP_PORT"))); err != nil {
+		log.Panic(err)
+	}
 }
